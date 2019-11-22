6d494b12a494eb797c534625176c34a4126ceac3
WenHY <39972162+xdwenhaoyu@users.noreply.github.com>
2019-11-22 16:44:25 +0800

    MavenSSM

--- 代码/SSMBasics/src/testSpring/AddressService.java
+++ 代码/SSMBasics/src/testSpring/AddressService.java
@@ -2,9 +2,6 @@ package testSpring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.transaction.annotation.Transactional;
-
 import dbexample.Address;
 import dbexample.AddressMapper;
 
